8e851aa92db638e36f5c549cae50ac52c84faab6
Graham McIntire <gmcintire@getaroom.com>
2020-01-11 19:02:58 -0600

    change entrypoint to absolute paths

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 docker build -t aprsme/app .
 docker push aprsme/app
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-./app/bin/aprsme eval Aprsme.Release.migrate
-./app/bin/aprsme start
+/app/bin/aprsme eval Aprsme.Release.migrate
+/app/bin/aprsme start
